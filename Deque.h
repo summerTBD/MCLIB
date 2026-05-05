59711a195a9795aba67652927b7b3f6d7fcac95e
summerTBD <hhl1973112ww@outlook.com>
2026-05-05 16:51:26 +0800

    style: normalize deque macro naming and parameter style to match Vec.h
    - Rename structs to `Deque_NAME` / `DequeOps_NAME` for consistency
    - Use `self` as the unified first parameter name (previously `dq`)
    - Move `ops` pointer to the top of the struct declaration
    - Reorder declarations: forward decls, ops table, instance, then impls
    - Keep all function signatures consistent with the Vec macro style

--- Deque.h
+++ Deque.h
@@ -9,49 +9,50 @@
 #endif
 
 #define DEFINE_DEQUE(TYPE, NAME)                                               \
-	/* ---------- ops 表前向声明 ---------- */                                 \
-	typedef struct deque_ops_##NAME deque_ops_##NAME;                          \
+	/* ---------- 前向声明 ---------- */                                       \
+	typedef struct Deque_##NAME Deque_##NAME;                                  \
+	typedef struct DequeOps_##NAME DequeOps_##NAME;                            \
                                                                                \
-	/* ---------- 双端队列结构体 ---------- */                                 \
-	typedef struct {                                                           \
+	/* ---------- 操作表结构 ---------- */                                     \
+	struct DequeOps_##NAME {                                                   \
+		void (*push_back)(Deque_##NAME * self, TYPE val);                      \
+		void (*push_front)(Deque_##NAME * self, TYPE val);                     \
+		TYPE (*pop_back)(Deque_##NAME * self);                                 \
+		TYPE (*pop_front)(Deque_##NAME * self);                                \
+		TYPE (*front)(const Deque_##NAME *self);                               \
+		TYPE (*back)(const Deque_##NAME *self);                                \
+		int (*empty)(const Deque_##NAME *self);                                \
+		size_t (*size)(const Deque_##NAME *self);                              \
+		void (*clear)(Deque_##NAME * self);                                    \
+		void (*destroy)(Deque_##NAME * self);                                  \
+	};                                                                         \
+                                                                               \
+	/* ---------- Deque 结构体 ---------- */                                   \
+	struct Deque_##NAME {                                                      \
+		const DequeOps_##NAME *ops;                                            \
 		TYPE *data;                                                            \
 		size_t capacity;                                                       \
 		size_t head;                                                           \
 		size_t tail;                                                           \
 		size_t size;                                                           \
-		const deque_ops_##NAME *ops;                                           \
-	} deque_##NAME;                                                            \
-                                                                               \
-	/* ---------- ops 表完整定义 ---------- */                                 \
-	struct deque_ops_##NAME {                                                  \
-		void (*push_back)(deque_##NAME *, TYPE);                               \
-		void (*push_front)(deque_##NAME *, TYPE);                              \
-		TYPE (*pop_back)(deque_##NAME *);                                      \
-		TYPE (*pop_front)(deque_##NAME *);                                     \
-		TYPE (*front)(const deque_##NAME *);                                   \
-		TYPE (*back)(const deque_##NAME *);                                    \
-		int (*empty)(const deque_##NAME *);                                    \
-		size_t (*size)(const deque_##NAME *);                                  \
-		void (*clear)(deque_##NAME *);                                         \
-		void (*destroy)(deque_##NAME *);                                       \
 	};                                                                         \
                                                                                \
-	/* ---------- 函数前向声明（供 ops 实例使用） ---------- */                \
-	static void deque_##NAME##_push_back(deque_##NAME *, TYPE);                \
-	static void deque_##NAME##_push_front(deque_##NAME *, TYPE);               \
-	static TYPE deque_##NAME##_pop_back(deque_##NAME *);                       \
-	static TYPE deque_##NAME##_pop_front(deque_##NAME *);                      \
-	static TYPE deque_##NAME##_front(const deque_##NAME *);                    \
-	static TYPE deque_##NAME##_back(const deque_##NAME *);                     \
-	static int deque_##NAME##_empty(const deque_##NAME *);                     \
-	static size_t deque_##NAME##_size(const deque_##NAME *);                   \
-	static void deque_##NAME##_clear(deque_##NAME *);                          \
-	static void deque_##NAME##_destroy(deque_##NAME *);                        \
-	static void deque_##NAME##_grow(deque_##NAME *);                           \
-	static void deque_##NAME##_init(deque_##NAME *, size_t);                   \
+	/* ====== 所有 static 函数前向声明 ====== */                               \
+	static void deque_##NAME##_push_back(Deque_##NAME *self, TYPE val);        \
+	static void deque_##NAME##_push_front(Deque_##NAME *self, TYPE val);       \
+	static TYPE deque_##NAME##_pop_back(Deque_##NAME *self);                   \
+	static TYPE deque_##NAME##_pop_front(Deque_##NAME *self);                  \
+	static TYPE deque_##NAME##_front(const Deque_##NAME *self);                \
+	static TYPE deque_##NAME##_back(const Deque_##NAME *self);                 \
+	static int deque_##NAME##_empty(const Deque_##NAME *self);                 \
+	static size_t deque_##NAME##_size(const Deque_##NAME *self);               \
+	static void deque_##NAME##_clear(Deque_##NAME *self);                      \
+	static void deque_##NAME##_destroy(Deque_##NAME *self);                    \
+	static void deque_##NAME##_grow(Deque_##NAME *self);                       \
+	static void deque_##NAME##_init(Deque_##NAME *self, size_t init_cap);      \
                                                                                \
-	/* ---------- 全局 ops 表实例 ---------- */                                \
-	static const deque_ops_##NAME deque_##NAME##_ops = {                       \
+	/* ---------- 默认操作表实例 ---------- */                                 \
+	static const DequeOps_##NAME deque_##NAME##_ops = {                        \
 		.push_back = deque_##NAME##_push_back,                                 \
 		.push_front = deque_##NAME##_push_front,                               \
 		.pop_back = deque_##NAME##_pop_back,                                   \
@@ -63,95 +64,93 @@
 		.clear = deque_##NAME##_clear,                                         \
 		.destroy = deque_##NAME##_destroy};                                    \
                                                                                \
-	/* ---------- 内部扩容 ---------- */                                       \
-	static void deque_##NAME##_grow(deque_##NAME *dq) {                        \
+	/* ---------- 函数实现 ---------- */                                       \
+	static void deque_##NAME##_grow(Deque_##NAME *self) {                      \
 		size_t new_cap =                                                       \
-			dq->capacity == 0 ? DEQUE_DEFAULT_CAPACITY : dq->capacity * 2;     \
+			self->capacity == 0 ? DEQUE_DEFAULT_CAPACITY : self->capacity * 2; \
 		TYPE *new_data = (TYPE *)malloc(new_cap * sizeof(TYPE));               \
 		assert(new_data != NULL);                                              \
-		for (size_t i = 0; i < dq->size; ++i)                                  \
-			new_data[i] = dq->data[(dq->head + i) % dq->capacity];             \
-		free(dq->data);                                                        \
-		dq->data = new_data;                                                   \
-		dq->head = 0;                                                          \
-		dq->tail = dq->size;                                                   \
-		dq->capacity = new_cap;                                                \
+		for (size_t i = 0; i < self->size; ++i)                                \
+			new_data[i] = self->data[(self->head + i) % self->capacity];       \
+		free(self->data);                                                      \
+		self->data = new_data;                                                 \
+		self->head = 0;                                                        \
+		self->tail = self->size;                                               \
+		self->capacity = new_cap;                                              \
 	}                                                                          \
                                                                                \
-	/* ---------- 初始化 ---------- */                                         \
-	static void deque_##NAME##_init(deque_##NAME *dq, size_t init_cap) {       \
+	static void deque_##NAME##_init(Deque_##NAME *self, size_t init_cap) {     \
 		if (init_cap == 0)                                                     \
 			init_cap = DEQUE_DEFAULT_CAPACITY;                                 \
-		dq->data = (TYPE *)malloc(init_cap * sizeof(TYPE));                    \
-		assert(dq->data != NULL);                                              \
-		dq->capacity = init_cap;                                               \
-		dq->head = 0;                                                          \
-		dq->tail = 0;                                                          \
-		dq->size = 0;                                                          \
-		dq->ops = &deque_##NAME##_ops;                                         \
+		self->data = (TYPE *)malloc(init_cap * sizeof(TYPE));                  \
+		assert(self->data != NULL);                                            \
+		self->capacity = init_cap;                                             \
+		self->head = 0;                                                        \
+		self->tail = 0;                                                        \
+		self->size = 0;                                                        \
+		self->ops = &deque_##NAME##_ops;                                       \
 	}                                                                          \
                                                                                \
-	/* ---------- 销毁 ---------- */                                           \
-	static void deque_##NAME##_destroy(deque_##NAME *dq) {                     \
-		free(dq->data);                                                        \
-		dq->data = NULL;                                                       \
-		dq->capacity = dq->head = dq->tail = dq->size = 0;                     \
+	static void deque_##NAME##_destroy(Deque_##NAME *self) {                   \
+		free(self->data);                                                      \
+		self->data = NULL;                                                     \
+		self->capacity = self->head = self->tail = self->size = 0;             \
 	}                                                                          \
                                                                                \
-	static void deque_##NAME##_push_back(deque_##NAME *dq, TYPE val) {         \
-		if (dq->size == dq->capacity)                                          \
-			deque_##NAME##_grow(dq);                                           \
-		dq->data[dq->tail] = val;                                              \
-		dq->tail = (dq->tail + 1) % dq->capacity;                              \
-		dq->size++;                                                            \
+	static void deque_##NAME##_push_back(Deque_##NAME *self, TYPE val) {       \
+		if (self->size == self->capacity)                                      \
+			deque_##NAME##_grow(self);                                         \
+		self->data[self->tail] = val;                                          \
+		self->tail = (self->tail + 1) % self->capacity;                        \
+		self->size++;                                                          \
 	}                                                                          \
                                                                                \
-	static void deque_##NAME##_push_front(deque_##NAME *dq, TYPE val) {        \
-		if (dq->size == dq->capacity)                                          \
-			deque_##NAME##_grow(dq);                                           \
-		dq->head = (dq->head - 1 + dq->capacity) % dq->capacity;               \
-		dq->data[dq->head] = val;                                              \
-		dq->size++;                                                            \
+	static void deque_##NAME##_push_front(Deque_##NAME *self, TYPE val) {      \
+		if (self->size == self->capacity)                                      \
+			deque_##NAME##_grow(self);                                         \
+		self->head = (self->head - 1 + self->capacity) % self->capacity;       \
+		self->data[self->head] = val;                                          \
+		self->size++;                                                          \
 	}                                                                          \
                                                                                \
-	static TYPE deque_##NAME##_pop_back(deque_##NAME *dq) {                    \
-		assert(dq->size > 0);                                                  \
-		dq->tail = (dq->tail - 1 + dq->capacity) % dq->capacity;               \
-		TYPE ret = dq->data[dq->tail];                                         \
-		dq->size--;                                                            \
+	static TYPE deque_##NAME##_pop_back(Deque_##NAME *self) {                  \
+		assert(self->size > 0);                                                \
+		self->tail = (self->tail - 1 + self->capacity) % self->capacity;       \
+		TYPE ret = self->data[self->tail];                                     \
+		self->size--;                                                          \
 		return ret;                                                            \
 	}                                                                          \
                                                                                \
-	static TYPE deque_##NAME##_pop_front(deque_##NAME *dq) {                   \
-		assert(dq->size > 0);                                                  \
-		TYPE ret = dq->data[dq->head];                                         \
-		dq->head = (dq->head + 1) % dq->capacity;                              \
-		dq->size--;                                                            \
+	static TYPE deque_##NAME##_pop_front(Deque_##NAME *self) {                 \
+		assert(self->size > 0);                                                \
+		TYPE ret = self->data[self->head];                                     \
+		self->head = (self->head + 1) % self->capacity;                        \
+		self->size--;                                                          \
 		return ret;                                                            \
 	}                                                                          \
                                                                                \
-	static TYPE deque_##NAME##_front(const deque_##NAME *dq) {                 \
-		assert(dq->size > 0);                                                  \
-		return dq->data[dq->head];                                             \
+	static TYPE deque_##NAME##_front(const Deque_##NAME *self) {               \
+		assert(self->size > 0);                                                \
+		return self->data[self->head];                                         \
 	}                                                                          \
                                                                                \
-	static TYPE deque_##NAME##_back(const deque_##NAME *dq) {                  \
-		assert(dq->size > 0);                                                  \
-		return dq->data[(dq->tail - 1 + dq->capacity) % dq->capacity];         \
+	static TYPE deque_##NAME##_back(const Deque_##NAME *self) {                \
+		assert(self->size > 0);                                                \
+		return self->data[(self->tail - 1 + self->capacity) % self->capacity]; \
 	}                                                                          \
                                                                                \
-	static int deque_##NAME##_empty(const deque_##NAME *dq) {                  \
-		return dq->size == 0;                                                  \
+	static int deque_##NAME##_empty(const Deque_##NAME *self) {                \
+		return self->size == 0;                                                \
 	}                                                                          \
                                                                                \
-	static size_t deque_##NAME##_size(const deque_##NAME *dq) {                \
-		return dq->size;                                                       \
+	static size_t deque_##NAME##_size(const Deque_##NAME *self) {              \
+		return self->size;                                                     \
 	}                                                                          \
                                                                                \
-	static void deque_##NAME##_clear(deque_##NAME *dq) {                       \
-		dq->head = 0;                                                          \
-		dq->tail = 0;                                                          \
-		dq->size = 0;                                                          \
+	static void deque_##NAME##_clear(Deque_##NAME *self) {                     \
+		self->head = 0;                                                        \
+		self->tail = 0;                                                        \
+		self->size = 0;                                                        \
 	}
 
 #endif /* DEQUE_H */
--- test_file.c
+++ test_file.c
@@ -5,7 +5,7 @@
 DEFINE_DEQUE(int, dq)
 
 int main() {
-	deque_dq a;
+	Deque_dq a;
 	deque_dq_init(&a, 1);
 	a.ops->push_back(&a, 1);
 	printf("%d\n", a.ops->back(&a));
